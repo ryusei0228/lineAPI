c05acf1a660308bcaa67bff973845eb4716a33b4
Hidaka Ryu <tensyo.sunflower@gmail.com>
2022-02-12 19:41:47 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask,request,abort
+from linebot import LineBotApi,WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent,TextMessage,TextSendMessage
+import os
+import requests
+import pprint
+import pya3rt
+
+app=Flask(__name__)
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+line_bot_api=LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler=WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback",methods=["POST"])
+def callback():
+    signature=request.headers["X-Line-Signature"]
+
+    body=request.get_data(as_text=True)
+    app.logger.info("Request body"+body)
+
+    try:
+        handler.handle(body,signature)
+    except InvalidSignatureError:
+        abort(400)
+    return "OK"
+
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    receive_text = event.message.text
+
+    reply_text = talkapi_response(recieve_text)
+
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply_text))
+
+def talkapi_response(text):
+    apikey = "DZZwfFfsz0009pdhB8SDfw3P1luSANim"
+    client = pya3rt.TalkClient(apikey)
+    response = client.talk(text)
+    return ((response['results'])[0])['reply']
+
+if __name__=="__main__":
+    port=int(os.getenv("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
